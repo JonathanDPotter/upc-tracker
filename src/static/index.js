41b8d3ff10b9caaf0b54627933129348b8df9c0b
Jonathan Potter <jonathan.d.potter@outlook.com>
2024-01-20 05:22:37 -0500

    Updated routes display on index page

--- src/static/index.js
+++ src/static/index.js
@@ -28,59 +28,19 @@ class Footer extends HTMLElement {
   }
 }
 
+class Routes extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  async connectedCallback() {
+    const routes = await (
+      await fetch("https://upc-tracker.herokuapp.com/routes")
+    ).json();
+    this.innerHTML = `<pre>${JSON.stringify(routes, undefined, 2)}</pre>`;
+  }
+}
+
+customElements.define("routes-component", Routes);
 customElements.define("header-component", Header);
 customElements.define("footer-component", Footer);
-
-const routes = {
-  user: {
-    ROOT: "/api/user",
-    GET: {
-      "/validate": "validates user by bearer token",
-      "/": "returns all user records",
-      "/:_id": "returns user record with specific _id",
-    },
-    POST: {
-      "/register": "creates a new user from {username, password}",
-      "/login": "returns validation token from {username, password}",
-    },
-    put: { "/:_id": "updates user record with specific _id" },
-    delete: { "/:_id": "deletes user with specific _id" },
-  },
-  group: {
-    ROOT: "/api/group",
-    GET: {
-      "/": "returns all group records",
-      "/:_id": "returns group record with specific _id",
-    },
-    POST: {
-      "/": "creates a new group record with {title, upcs}",
-    },
-    PUT: { "/:_id": "updates group record with specific _id" },
-    DELETE: { "/:_id": "deletes group with specific _id" },
-  },
-  index: {
-    ROOT: "/",
-    GET: {
-      "/": "returns the home page",
-      "/about": "returns the about page",
-      "/routes": "returns this json object",
-      "/healtcheck": "returns an 'OK' statuscode if the server is running",
-    },
-  },
-};
-
-const routesContainer = document.getElementsByClassName("routes");
-
-const keys = Object.keys(routes);
-
-const values = Object.values(routes);
-
-routesContainer[0].innerHTML = keys.map(
-  (key, i) =>
-    `<h3>${key}:</h3> <p class="route-list">${JSON.stringify(values[i])
-      .replace(/},/g, "}, <br />")
-      .replace(/{/g, "{ <br />")
-      .replace(/",/g, '", <br />')
-      .replace(/}/g, "} <br/> }")
-      .replace(/:/g, " : ")}</p>`
-);
